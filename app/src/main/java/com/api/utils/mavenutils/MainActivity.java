a890cfdc5d378da5c9fc6b9c71a00feee111f181
fujian.yu <fujian.yu@joyreach.com>
2018-07-06 14:24:11 +0800

    **初始化提交**

---
+++ app/src/main/java/com/api/utils/mavenutils/MainActivity.java
@@ -0,0 +1,16 @@
+package com.api.utils.mavenutils;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.api.utils.mutils.RandomUtil;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        RandomUtil.rand()
+    }
+}
